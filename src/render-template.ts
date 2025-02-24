cb39fbdda49160589043748e62512dfeed9d0ee3
vrugtehagel <vrugtehagel@pm.me>
2025-02-24 15:38:23 +0100

    Cache template rendering as optimization

--- src/render-template.ts
+++ src/render-template.ts
@@ -1,11 +1,18 @@
 import { RenderPlugin } from "npm:@11ty/eleventy@^3.0.0-alpha.15";
 
+const cache = new Map<string, Promise<(data: any) => string>>();
+
 export async function renderTemplate(
   template: string | { lang: string; source: string },
   data: { headers: Array<{ id: string; text: string; tag: string }> },
 ): Promise<string> {
-  const renderer = typeof template == "string"
-    ? await RenderPlugin.File(template)
-    : await RenderPlugin.String(template.source, template.lang);
+  const key = JSON.stringify(template);
+  const cached = cache.get(key);
+  if (cached) return await cached.then((renderer) => renderer(data));
+  const promise = typeof template == "string"
+    ? RenderPlugin.File(template)
+    : RenderPlugin.String(template.source, template.lang);
+  cache.set(key, promise);
+  const renderer = await promise;
   return await renderer(data);
 }
